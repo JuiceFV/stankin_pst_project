9ffd0ede2b6825390d6d1e43f07e3dc976a0413c
JuiceFV <aleksandr.juicefv@gmail.com>
2021-04-11 18:33:42 +0300

    added comments

--- application/sources/config_handler.py
+++ application/sources/config_handler.py
@@ -11,17 +11,21 @@ def upload_config(cfg_file=None):
     :param cfg_file: passed config-file.
     :return: config-dict.
     """
+    # Set the default path to the configuration file.
+    # Technically this path looks like <root directory>/config.yaml
     default_file = Path(__file__).parent.parent.parent / 'config.yaml'
-    print(default_file)
+
+    # Using PyYaml read the configuration file and store to to the dict.
     with open(default_file, 'r') as f:
         config = yaml.safe_load(f)
 
+    # In the case an user passed custom configuration file. Read it and update the default one.
     cfg_dict = {}
-    # if configuration file exists then upload the config as dict.
+    # if custom configuration file exists then upload the config as dict.
     if cfg_file:
         cfg_dict = yaml.safe_load(cfg_file)
 
-    # if dict doesn't empty then update the config.
+    # if dict doesn't empty then update the main config.
     if cfg_dict:
         config.update(**cfg_dict)
 
