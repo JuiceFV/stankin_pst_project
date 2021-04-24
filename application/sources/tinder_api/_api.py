0c54ac2d6f6a50d3edd4dd86cbd973a57811ac51
TriplG <sasha-aleyynik@mail.ru>
2021-04-24 16:42:47 +0300

    base api done

--- application/sources/tinder_api/_api.py
+++ application/sources/tinder_api/_api.py
@@ -116,3 +116,184 @@ class TinderAPI:
         :param url: a requested link
         :return: response"""
         return self._request("delete", url)
+
+    def recs(self, limit=10):
+        """Request for stack of girls or boys, depends in you.
+        :param limit: limit for request
+        :return: response
+        """
+        return self._post(globals.KEY_ENDPOINTS['recs']['endpoint'], data={'limit': limit})
+
+    def profile(self):
+        """Get my/your user's profile
+        :return: response
+        """
+        return self._get(globals.KEY_ENDPOINTS['profile']['endpoint'])
+
+    def updates(self, since):
+        """Last updates since <since>.
+        :param since: date when to look for updates
+        :return: response
+        """
+        return self._post(globals.KEY_ENDPOINTS['updates']['endpoint'], {"last_activity_date": since} if since else {})
+
+    def meta(self):
+        """Retrieve my/your own metadata. (swipes left, people seen, etc..)
+        :return: response
+        """
+        return self._get(globals.KEY_ENDPOINTS['meta']['endpoint'])
+
+    def add_profile_photo(self, facebook_id, x_dist, y_dist, x_offset, y_offset):
+        """Adds a profile's photo to our Tinder profile.
+        :param facebook_id: your facebook id.
+        :param x_dist: x-axis distance (percent)
+        :param y_dist: y-axis distance (percent)
+        :param x_offset: x-axis offset (percent)
+        :param y_offset: y-axis offset (percent)
+        """
+        data = {
+            "transmit": "fb",
+            "assets": [
+                {
+                    "id": str(facebook_id),
+                    "xdistance_percent": float(x_dist),
+                    "ydistance_percent": float(y_dist),
+                    "xoffset_percent": float(x_offset),
+                    "yoffset_percent": float(y_offset)
+                }
+            ]
+        }
+
+        return self._request("post", globals.CONTENT_HOST + globals.KEY_ENDPOINTS['meta']['endpoint'], data=data)
+
+    def delete_profile_photo(self, photo_id):
+        """Delete profile photo from Tinder profile.
+        :param photo_id: id of deleting photo.
+        :return: response
+        """
+        data = {"assets": [photo_id]}
+
+        return self._request("delete", globals.CONTENT_HOST + globals.KEY_ENDPOINTS['meta']['endpoint'], data=data)
+
+    def matches(self, since):
+        """Check how many matches you have.
+        :param since: Search for the updates since to
+        :return: found matches
+        """
+        return self.updates(since)['matches']
+
+    def update_profile(self, profile):
+        """Update your profile.
+        :param profile: updated profile
+        :return: response
+        """
+        return self._post(globals.KEY_ENDPOINTS['profile']['endpoint'], profile)
+
+    def like(self, user):
+        """Like an user.
+        :param user: user which to like
+        :return: response
+        """
+        return self._get(f"/like/{user}")
+
+    def dislike(self, user):
+        """Dislike an profile.
+        :param user: user which to like
+        :return: response
+        """
+        return self._get(f"/pass/{user}")
+
+    def message(self, user, body):
+        """Message to an user.
+        :param user: an user to message to
+        :param body: the body of a message
+        :return: response
+        """
+        return self._post(f"/user/matches/{user}", {"message": str(body)})
+
+    def message_gif(self, user, giphy_id):
+        """Message to an user with a gif.
+        :param user: an user to message to
+        :param giphy_id: the id of a gif
+        :return: response
+        """
+        return self._post(f"/user/matches/{user}", {"type": "gif", "gif_id": str(giphy_id)})
+
+    def report(self, user, cause=globals.ReportCause.Other, text=""):
+        """Report an user.
+        :param user: an user to message to
+        :param cause: the class of report's causes:
+        Other/Spam/Inappropriate_Photos/Bad_Offline_Behavior/Inappropriate_Messages
+        (Other by default)
+        :param text: the additional message to a report. ("" by default)
+        :return: response
+        """
+        try:
+            cause = int(cause)
+        except TypeError:
+            cause = int(cause.value)
+
+        data = {"cause": cause}
+
+        if text and globals.ReportCause(cause) == globals.ReportCause.Other:
+            data["text"] = text
+
+        return self._post("/report/" + user, data)
+
+    def user_info(self, user_id):
+        """Obtain all information about an user
+        :param user_id: an user's id
+        :return: response
+        """
+        return self._get("/user/" + user_id)
+
+    def ping(self, lat, lon):
+        """Particularly update a profile's location
+        :param lat: latitude
+        :param lon: longitude
+        :return: response
+        """
+        return self._post("/user/ping", {"lat": lat, "lon": lon})
+
+    def share(self, user):
+        """Share an user profile.
+        :param user: an user
+        :return: response
+        """
+        return self._post(f"/user/{user}/share")
+
+    def superlike(self, user):
+        """Superlike an user.
+        :param user: an user
+        :return: response
+        """
+        result = self._post(f"/like/{user}/super")
+        if 'limit_exceeded' in result and result['limit_exceeded']:
+            raise errors.RequestError("Superlike limit exceeded")
+        return result
+
+    def fb_friends(self):
+        """Requests records of all facebook friends using Tinder Social.
+        :return: object containing array of all friends who use Tinder Social.
+        """
+        return self._get("/group/friends")
+
+    def like_message(self, message):
+        """Hearts a message sent by a match
+        :param message: message id
+        :return: empty json, response code is 201 (Created)
+        """
+        return self._post(f"/message/{message.id}/like")
+
+    def unlike_message(self, message):
+        """Removes heart from a message
+        :param message: message id
+        :return: empty json, response code is 204 (No content)
+        """
+        return self._delete(f"/message/{message.id}/like")
+
+    def liked_messages(self, since):
+        """Obtain liked message.
+        :return: liked messages
+        """
+        return self.updates(since)['liked_messages']
