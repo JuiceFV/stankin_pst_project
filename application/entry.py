2ff6eff1268469ca7b0520579f5674a9ce6bc49c
JuiceFV <alexcar19622000@gmail.com>
2021-05-26 14:02:26 +0300

    mcc metric added

--- application/entry.py
+++ application/entry.py
@@ -19,6 +19,24 @@ import matplotlib.pyplot as plt
 import argparse
 from path import Path
 
+def mcc(y_true, y_pred):
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    y_pred_neg = 1 - y_pred_pos
+
+    y_pos = K.round(K.clip(y_true, 0, 1))
+    y_neg = 1 - y_pos
+
+    tp = K.sum(y_pos * y_pred_pos)
+    tn = K.sum(y_neg * y_pred_neg)
+
+    fp = K.sum(y_neg * y_pred_pos)
+    fn = K.sum(y_pos * y_pred_neg)
+
+    numerator = (tp * tn - fp * fn)
+    denominator = K.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
+
+    return numerator / (denominator + K.epsilon())
+
 parser = argparse.ArgumentParser(description="Tinder-Bot user's validator")
 parser.add_argument(
                     '-c',
@@ -173,8 +191,11 @@ def swipe(_session, compiled_model):
                 print("No photo has been obtained")
 
 
+dependencies = {
+    'mcc': mcc
+}
 session = pytinder.Session(config=cfg['session'])
 model_path = Path(__file__).parent / cfg['model']['path']
 print(model_path)
-model = load_model(model_path)
+model = load_model(model_path,custom_objects=dependencies)
 swipe(session, model)
